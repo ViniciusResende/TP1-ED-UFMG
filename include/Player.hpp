2b43c03cebf9c80bd201b5ca5983aa3b0a7db3bb
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-05-21 15:47:28 -0300

    feat: implementing Player class

---
+++ include/Player.hpp
@@ -0,0 +1,32 @@
+#ifndef HAND_DEFAULT_SIZE
+#define HAND_DEFAULT_SIZE 5
+#endif
+
+#ifndef PLAYERH
+#define PLAYERH
+
+#include "memlog.h"
+#include <string>
+
+#include "Card.hpp"
+
+class Player {
+  public:  
+    Player(std::string name, double initialMoney, Card initialHand[HAND_DEFAULT_SIZE]);
+
+    void setPlayerHand(Card initialHand[HAND_DEFAULT_SIZE]);
+    Card getPlayerCardByIndex(int idx);
+    void setMoney(double value);
+    double getMoney();
+
+    ~Player();
+
+  private:
+    static int _id;
+    int id;
+    std::string name;
+    double money;
+    Card hand[HAND_DEFAULT_SIZE];
+};
+
+#endif
---
+++ src/Player.cpp
@@ -0,0 +1,57 @@
+#include "Player.hpp"
+#include "msgassert.h"
+
+int Player::_id = 0;
+
+Player::Player(std::string name, double initialMoney, Card initialHand[HAND_DEFAULT_SIZE]) {
+  errorAssert(!name.empty(), "Invalid name provided");
+  errorAssert(initialMoney >= 0, "Initial money can't be negative");
+  
+  this->_id += 1;
+  this->id = this->_id;
+
+  this->name = name;
+  WRITEMEMLOG((long int) (&(this->name)), sizeof(std::string), this->id);
+  
+  this->money = initialMoney;
+  WRITEMEMLOG((long int) (&(this->money)), sizeof(double), this->id);
+
+
+  for(int i = 0; i < HAND_DEFAULT_SIZE; i++) {
+    this->hand[i] = initialHand[i];
+    WRITEMEMLOG((long int) (&(this->hand[i])), sizeof(double), this->id);
+  }
+}
+
+void Player::setPlayerHand(Card currentHand[HAND_DEFAULT_SIZE]) {
+  for(int i = 0; i < HAND_DEFAULT_SIZE; i++) {
+    this->hand[i] = currentHand[i];
+    WRITEMEMLOG((long int) (&(this->hand[i])), sizeof(Card), this->id);
+  }
+}
+
+Card Player::getPlayerCardByIndex(int idx) {
+  errorAssert((idx >= 0) && (idx < HAND_DEFAULT_SIZE), "Invalid hand index");
+  READMEMLOG((long int) (&(this->hand[idx])), sizeof(Card), this->id);
+
+  return this->hand[idx];
+}
+
+void Player::setMoney(double value) {
+  errorAssert(value >= 0, "Money value can't be negative");
+
+  this->money = value;
+  WRITEMEMLOG((long int) (&(this->money)), sizeof(double), this->id);
+}
+
+double Player::getMoney() {
+  READMEMLOG((long int) (&(this->money)), sizeof(double), this->id);
+  return this->money;
+}
+
+Player::~Player() {
+  warnAssert(this->id != -1, "Player instance has already been destroyed");
+
+  this->money = this->id = -1;
+  this->name = "";
+}
