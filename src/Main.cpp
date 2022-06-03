8d2de4b3f27e54e3b74f3a54ca228640ba6352c7
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-06-03 16:25:44 -0300

    feat: refactoring main file structure implementing factory paradigm

--- src/Main.cpp
+++ src/Main.cpp
@@ -14,6 +14,7 @@
 #include "Game.hpp"
 
 #define DEFAULT_INPUT_FILE "../assets/entrada.txt"
+#define DEFAULT_OUTPUT_FILE "../assets/saida.txt"
 
 struct ConfigStruct {
   bool regmem;
@@ -54,19 +55,89 @@ void parse_args(int argc,char ** argv) {
     }
 }
 
-int main(int argc, char ** argv) {
-  parse_args(argc,argv);
+Vector<Card>* handOfCardsFactory(std::ifstream &inputFile, std::string buffer) {
+  Vector<Card> *playerHand = new Vector<Card>(HAND_DEFAULT_SIZE);
 
-  startMemLog(config.logname);
+  for (int k = 0; k < HAND_DEFAULT_SIZE; k++) {
+    std::string cardSpecification;
+    inputFile >> cardSpecification;
+    
+    char cardSuit = toupper(cardSpecification.back());
+    cardSpecification.pop_back();
 
-  if (config.regmem) 
-    activateMemLog();
-  else 
-    deactivateMemLog();
+    int cardNumber = stoi(cardSpecification);
 
+    Card currentCard = Card(cardNumber, cardSuit);
+    playerHand->setElement(k, currentCard);
+  }
+  
+  playerHand->sortVector();
+
+  return playerHand;
+}
+
+Player* playerFactory(std::ifstream &inputFile, Game* masterGame, int initialPlayerMoneyAmount, std::string buffer) {
+  std::string name;
+  int betValue;
+
+  inputFile >> buffer;
+  while (!std::isdigit(buffer[0])) {
+    name.append(buffer);
+    name.append(" ");
+    inputFile >> buffer;
+  }
+  name.pop_back();
+
+  betValue = stoi(buffer);
+
+  Vector<Card> *playerHand = handOfCardsFactory(inputFile, buffer);
+
+  Player *currentPlayer = masterGame->retrievePlayerInstance(name, initialPlayerMoneyAmount, betValue, playerHand);
+
+  return currentPlayer;
+}
+
+Match matchFactory(std::ifstream &inputFile, std::ofstream &outFile, Game* masterGame, int initialPlayerMoneyAmount, std::string buffer) {
+  int numberOfPlayers, minimumAmountToPlay;
+
+  inputFile >> buffer;
+  numberOfPlayers = stoi(buffer);
+  inputFile >> buffer;
+  minimumAmountToPlay = stoi(buffer);
+
+  Match currentMatch = Match(numberOfPlayers, minimumAmountToPlay);
+
+  for (int j = 0; j < numberOfPlayers; j++) {
+    Player *currentPlayer = playerFactory(inputFile, masterGame, initialPlayerMoneyAmount, buffer);
+    currentMatch.addPlayerToMatch(currentPlayer);
+  }
+
+  masterGame->chargeTheMinimunAmountToPlay(minimumAmountToPlay);
+  currentMatch.getGameResult(masterGame->inGamePlayers->length(), outFile);    
+  currentMatch.printMatch();
+
+  return currentMatch;
+}
+
+void printGameResult(std::ofstream &outFile, Game* masterGame) {
+  outFile << "####" << std::endl;
+
+  Match firstMatch = masterGame->inGameMatches->getElement(0);
+  errorAssert(firstMatch.inMatchPlayers->length() == masterGame->inGamePlayers->length(), "Not all game players have participated in first match");
+  firstMatch.inMatchPlayers->sortVectorDesc();
+
+
+  Player* it;
+  for (int i = 0; i < firstMatch.inMatchPlayers->length(); i++) {
+    it = firstMatch.inMatchPlayers->getElement(i);
+    outFile << it->getName() << " " << it->getMoney() << std::endl;
+  }  
+}
+
+void runGameOrchestrator() {
   std::ifstream inputFile(DEFAULT_INPUT_FILE);
   errorAssert(inputFile.is_open(), "\nFailed to open input file");
-  std::ofstream outFile("saida.txt");
+  std::ofstream outFile(DEFAULT_OUTPUT_FILE);
   errorAssert(outFile.is_open(), "\nFailed to open output file");
 
   std::string buffer;
@@ -77,68 +148,33 @@ int main(int argc, char ** argv) {
   inputFile >> buffer;
   initialPlayerMoneyAmount = stoi(buffer);
 
-  Game masterGame = Game(numberOfMatches);
+  Game *masterGame = new Game(numberOfMatches);
 
   for (int i = 0; i < numberOfMatches; i++) {
-    int numberOfPlayers, minimumAmountToPlay;
-    inputFile >> buffer;
-    numberOfPlayers = stoi(buffer);
-    inputFile >> buffer;
-    minimumAmountToPlay = stoi(buffer);
-
-    Match currentMatch = Match(numberOfPlayers, minimumAmountToPlay);
-
-    for (int j = 0; j < numberOfPlayers; j++) {
-      std::string name;
-      int betValue;
-
-      inputFile >> buffer;
-      while (!std::isdigit(buffer[0])) {
-        name.append(buffer);
-        name.append(" ");
-        inputFile >> buffer;
-      }
-      name.pop_back();
-
-      betValue = stoi(buffer);
-
-      Vector<Card> *playerHand = new Vector<Card>(HAND_DEFAULT_SIZE);
-
-      for (int k = 0; k < HAND_DEFAULT_SIZE; k++) {
-        std::string cardSpecification;
-        inputFile >> cardSpecification;
-
-        
-        char cardSuit = toupper(cardSpecification.back());
-        cardSpecification.pop_back();
-
-        int cardNumber = stoi(cardSpecification);
-
-        Card currentCard = Card(cardNumber, cardSuit);
-        playerHand->setElement(k, currentCard);
-      }
-      
-      playerHand->sortVector();
-      Player *currentPlayer = masterGame.retrievePlayerInstance(name, initialPlayerMoneyAmount, betValue, playerHand);
-      currentMatch.addPlayerToMatch(currentPlayer);
-    }
-
-    masterGame.chargeTheMinimunAmountToPlay(minimumAmountToPlay);
-    currentMatch.getGameResult(masterGame.inGamePlayers->length(), outFile);    
-    currentMatch.printMatch();
-    masterGame.addMatchToGame(currentMatch);
+    Match currentMatch = matchFactory(inputFile, outFile, masterGame, initialPlayerMoneyAmount, buffer);
+    masterGame->addMatchToGame(currentMatch);
   }
 
-  outFile << "####" << std::endl;
-  masterGame.inGameMatches->getElement(0).inMatchPlayers->sortVectorDesc();
-  Player* it;
-  for (int i = 0; i < masterGame.inGamePlayers->length(); i++) {
-    it = masterGame.inGameMatches->getElement(0).inMatchPlayers->getElement(i);
-    outFile << it->getName() << " " << it->getMoney() << std::endl;
-  }  
+  printGameResult(outFile, masterGame);
 
   outFile.close();
   inputFile.close();
+
+  warnAssert(!inputFile.is_open(), "\nFailed to close input file");
+  warnAssert(!outFile.is_open(), "\nFailed to close output file");
+}
+
+int main(int argc, char ** argv) {
+  parse_args(argc,argv);
+
+  startMemLog(config.logname);
+
+  if (config.regmem) 
+    activateMemLog();
+  else 
+    deactivateMemLog();
+
+  runGameOrchestrator();
 
   return endUpMemLog();
 }
